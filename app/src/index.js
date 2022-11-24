95e6f01b0398709d279a8da9788b5587e6a008d3
Koda28 <acovamartinez@gmail.com>
2022-11-23 20:13:19 -0600

    Init
    Preparación del proyecto

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Jsx = () =>{
+  return(
+    <>
+    </>
+  );
+};
+
+root.render(<Jsx/>);
