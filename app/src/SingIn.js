90b74d20a3c58cfe75e884d40612efce30aeeb8e
Koda28 <acovamartinez@gmail.com>
2022-11-27 12:55:35 -0600

    Forms
    Creación de formularos para Login y Singin

---
+++ app/src/SingIn.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+
+const FormSingIn = (props) => {
+    const[newUser, setNewUser] = useState("");
+    const[newPassword, setNewPassword] = useState("");
+    const[confirmPassword, setConfirmPassword] = useState("");
+
+    const onChange = (e) => {
+        console.log(e.target.name);
+        if(e.target.name === "newUser"){
+            setNewUser(e.target.value);
+        }
+        else if(e.target.name === "newPassword"){
+            setNewPassword(e.target.value);
+        }
+        else if(e.target.name === "confirmPassword"){
+            setConfirmPassword(e.target.value);
+        }
+    };
+
+    const onSubmit = (e) => {
+        console.log(e);
+        e.preventDefault();
+        if(newPassword === confirmPassword){
+            alert("Usuario creado correctamente");
+            //Falta ver lo de cambiar el true a iniciar secion 
+        }
+        else{
+            alert("Las contraseñas no coinciden");
+        }
+    };
+
+    return(
+        <>
+            <form action="" onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="newUser">Nuevo Usuario</label>
+                    <input 
+                        type="text" 
+                        name="newUser" 
+                        id="newUser" 
+                        value={newUser} 
+                        onChange={onChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="newPassword">Nueva Contraseña</label>
+                    <input 
+                        type="password" 
+                        name="newPassword" 
+                        id="newPassword" 
+                        value={newPassword} 
+                        onChange={onChange}></input>
+                </div>
+                <div>
+                    <label htmlFor="confirmPassword">Confirmar Contraseña</label>
+                    <input 
+                        type="password" 
+                        name="confirmPassword" 
+                        id="confirmPassword" 
+                        value={confirmPassword} 
+                        onChange={onChange}></input>
+                </div>
+                <button>Crear Usuario</button>
+            </form>
+        </>
+    );
+};
--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
+import FormLogin from './Login';
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const Jsx = () =>{
+  const[inicio, cambiarInicio] = useState(false);
+
   return(
     <>
+      {inicio === true ? (
+        <>
+          <h1>Bienvenido</h1>
+          <button onClick={() => cambiarInicio(false)}>Cerrar Sesion</button>
+        </>
+      ) : (
+        <>
+          <h1>Iniciar Sesion</h1>
+          <FormLogin cambiarInicio={cambiarInicio}/>
+        </>
+      )}
     </>
   );
 };
